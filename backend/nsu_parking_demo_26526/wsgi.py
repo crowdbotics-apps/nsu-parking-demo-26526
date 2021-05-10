c1f19c7f21a61dfc144e251b8eee3748b8b7eb26
Crowdbotics <team@crowdbotics.com>
2021-05-10 23:46:27 +0000

    Update models

--- backend/nsu_parking_demo_26526/wsgi.py
+++ backend/nsu_parking_demo_26526/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nsu_parking_demo_26526.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nsu_parking_demo_26526.settings")
 
 application = get_wsgi_application()
