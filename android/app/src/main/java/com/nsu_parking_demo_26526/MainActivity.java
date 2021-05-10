27f87e64c36a8cf04f479d8c5b0deda3efcf8d8c
Crowdbotics <team@crowdbotics.com>
2021-05-10 23:40:07 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/nsu_parking_demo_26526/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nsu_parking_demo_26526;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "nsu_parking_demo_26526";
+  }
+}
